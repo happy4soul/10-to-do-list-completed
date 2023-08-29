f16ca0e60d8a5d272706283e2afb6744c81e8d51
Bhaskar <bhaskarsrivastav24@gmail.com>
2023-08-29 16:01:00 +0530

    Updates

---
+++ precache-manifest.84749477c7eb4ad648626a28a5e13a7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03739ab1ea833169561968194e5bac7f",
+    "url": "/10-to-do-list-completed/index.html"
+  },
+  {
+    "revision": "6f837621e0beb9b436a9",
+    "url": "/10-to-do-list-completed/static/css/main.8247e105.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/10-to-do-list-completed/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/10-to-do-list-completed/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f837621e0beb9b436a9",
+    "url": "/10-to-do-list-completed/static/js/main.fc470ad4.chunk.js"
+  },
+  {
+    "revision": "cd06cc1dfb03e1591e26",
+    "url": "/10-to-do-list-completed/static/js/runtime-main.f642c97f.js"
+  }
+]);
